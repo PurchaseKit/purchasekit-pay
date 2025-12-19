17f4a3f2140d7a1e9c1d374f286bc6446a342d2d
Joe Masilotti <joe@masilotti.com>
2025-12-18 16:02:37 -0800

    Fix webhook registration

--- lib/purchasekit/pay/engine.rb
+++ lib/purchasekit/pay/engine.rb
@@ -7,6 +7,10 @@ module PurchaseKit
         ::Pay.enabled_processors << :purchasekit unless ::Pay.enabled_processors.include?(:purchasekit)
       end
 
+      initializer "purchasekit_pay.webhooks", after: :load_config_initializers do
+        ::Pay::Purchasekit.configure_webhooks
+      end
+
       initializer "purchasekit_pay.helpers" do
         ActiveSupport.on_load(:action_controller_base) do
           helper PurchaseKit::Pay::PaywallHelper
@@ -14,11 +18,10 @@ module PurchaseKit
       end
 
       initializer "purchasekit_pay.importmap", before: "importmap" do |app|
-        if defined?(Importmap)
+        if app.config.respond_to?(:importmap)
           app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+          app.config.importmap.cache_sweepers << root.join("app/javascript")
         end
-
-        app.config.importmap.cache_sweepers << root.join("app/javascript")
       end
 
       initializer "purchasekit_pay.assets" do |app|
