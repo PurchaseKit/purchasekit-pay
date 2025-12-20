58c3daa278e29b32a4279711a5e21fbb593ac052
Joe Masilotti <joe@masilotti.com>
2025-12-19 18:21:14 -0800

    Always broadcast stream when subscription updates

--- lib/purchasekit/pay/webhooks/subscription_updated.rb
+++ lib/purchasekit/pay/webhooks/subscription_updated.rb
@@ -2,6 +2,9 @@ module PurchaseKit
   module Pay
     module Webhooks
       class SubscriptionUpdated < Base
+        include ActionView::RecordIdentifier
+        include Turbo::Streams::ActionHelper
+
         def call(event)
           update_subscription(event,
             processor_plan: event["store_product_id"],
@@ -9,6 +12,18 @@ module PurchaseKit
             current_period_start: parse_time(event["current_period_start"]),
             current_period_end: parse_time(event["current_period_end"]),
             ends_at: parse_time(event["ends_at"])
+          )
+
+          broadcast_redirect(event) if event["success_path"].present?
+        end
+
+        private
+
+        def broadcast_redirect(event)
+          customer = ::Pay::Customer.find(event["customer_id"])
+          Turbo::StreamsChannel.broadcast_stream_to(
+            dom_id(customer),
+            content: turbo_stream_action_tag(:redirect, url: event["success_path"])
           )
         end
       end
